0fe65b3926937dd9ef48b7bfb5b44e14dbbf9207
ParastooSJ <76828711+ParastooSJ@users.noreply.github.com>
2022-06-30 12:33:24 -0400

    Create run.sh

---
+++ src/run.sh
@@ -0,0 +1,7 @@
+python random-sample-generator.py
+python sample-scorer.py
+python data-constructor.py
+python subgraph-pruning-trainer.py
+python subgraph-pruner.py
+python subgraph-truncate.py
+python subgraph-ranker.py
