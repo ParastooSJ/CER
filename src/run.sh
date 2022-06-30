77212629d1eae0918f969428fc556fb62561bcaa
ParastooSJ <76828711+ParastooSJ@users.noreply.github.com>
2022-06-30 12:46:19 -0400

    Delete src directory

--- src/run.sh
+++
@@ -1,7 +0,0 @@
-python random-sample-generator.py
-python sample-scorer.py
-python data-constructor.py
-python subgraph-pruning-trainer.py
-python subgraph-pruner.py
-python subgraph-truncate.py
-python subgraph-ranker.py
